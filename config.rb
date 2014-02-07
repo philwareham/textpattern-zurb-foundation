9f269873d86b9e64d28ef0078e93a72b4d6a3089
Phil Wareham <phil@designhive.com>
2014-02-07 15:49:21 +0000

    Update Compass config

--- config.rb
+++ config.rb
@@ -10,13 +10,19 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# The environment mode. Defaults to :production, can also be :development
+environment = :production
+
+# Chrome needs a precision of 7 to round properly
+Sass::Script::Number.precision = 7
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
