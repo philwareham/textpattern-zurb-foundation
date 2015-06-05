4d4001607b30231bab35ce72df9df0539d864042
Phil Wareham <phil@designhive.com>
2015-06-05 10:15:57 +0100

    Add Travis-CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,4 +94,5 @@ module.exports = function (grunt)
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('sass', ['compass']);
     grunt.registerTask('setup', ['shell:setup']);
+    grunt.registerTask('travis', ['compass']);
 };
